58e8d03de053a60e0982e142048d87a16374f9bc
patel-nancy <nancy.patel@wustl.edu>
2023-10-14 16:13:06 -0500

    change what you want

---
+++ background.js
@@ -0,0 +1,53 @@
+let googleUser = null;
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.action === 'authenticate') {
+      chrome.identity.getAuthToken({ interactive: true }, function(token) {
+        if (chrome.runtime.lastError) {
+          sendResponse({ success: false, message: chrome.runtime.lastError.message });
+          return;
+        }
+        // Validate the token and verify the account with Gmail API
+        verifyGoogleAccount(token).then(response => {
+          if (response.verified) {
+            googleUser = response.user;
+            // Send the verified account information to the server for MongoDB storage
+            sendToServer(googleUser);
+            sendResponse({ success: true, user: googleUser });
+          } else {
+            sendResponse({ success: false, message: 'Failed to verify the Google account.' });
+          }
+        });
+      });
+      return true; // Required to indicate that response will be sent asynchronously
+    }
+  });
+  
+  async function verifyGoogleAccount(token) {
+    // Call Gmail API to confirm the account authenticity
+    // Note: Real implementation would require proper error handling
+    let response = await fetch('https://www.googleapis.com/gmail/v1/users/me/profile', {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    });
+    let user = await response.json();
+    return {
+      verified: response.ok,
+      user: user.emailAddress // Assuming the user object contains the email address
+    };
+  }
+  
+  function sendToServer(user) {
+    // Send user data to your server where it will interact with MongoDB
+    // Ensure this connection is secure and your server handles data securely and ethically
+    fetch('https://yourserver.com/api/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    });
+    // Implement appropriate error handling for failed network requests
+  }
+  
--- popup.js
+++ popup.js
@@ -1,4 +1,3 @@
-//outside: create google cloud console, new project, enable gmail API, create OAuth 2.0 credentials
 //create backend server (Node.js), store email acc/access tokens
 //endpoints for auth, user management, accessing Gmail data via API
 
