7bfbda1285f7d51e264d4ea60e28929d07451aec
patel-nancy <nancy.patel@wustl.edu>
2023-10-14 12:48:55 -0500

    init manifest/ popup html/js

---
+++ popup.js
@@ -0,0 +1,5 @@
+//outside: create google cloud console, new project, enable gmail API, create OAuth 2.0 credentials
+//create backend server (Node.js), store email acc/access tokens
+//endpoints for auth, user management, accessing Gmail data via API
+
+//handle user auth using OAuth 2.0
