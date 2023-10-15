5f2aa391a02887a81b48784ddd8f87c07023b33a
patel-nancy <nancy.patel@wustl.edu>
2023-10-14 20:03:31 -0500

    token

---
+++ popup2.js
@@ -0,0 +1,25 @@
+window.onload = function() {
+    document.querySelector('button').addEventListener('click', function() {
+    chrome.identity.getAuthToken({interactive: true}, function(token) {
+    // console.log(token),
+    // console.log("encrypted token:"),
+    // encrypted_token = btoa(token),
+    // console.log(encrypted_token)
+    // console.log("decrypted"),
+    // console.log(atob(encrypted_token))
+    
+    
+    encrypted_token = btoa(token),
+    
+    
+    fetch('http://34.27.8.186:3000/', {
+    method: 'POST',
+    headers: {
+    'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(encrypted_token),
+    })
+    });
+    });
+    };
+    
